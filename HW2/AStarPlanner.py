5545312e1b09bb440acf8e9ba94044d16a09f4bc
ronen85 <ronen.nizri@gmail.com>
2019-12-10 14:17:15 +0200

    Add HW2

---
+++ HW2/AStarPlanner.py
@@ -0,0 +1,24 @@
+import sys
+import time
+import numpy
+
+class AStarPlanner(object):    
+    def __init__(self, planning_env):
+        self.planning_env = planning_env
+        self.nodes = dict()
+
+    def Plan(self, start_config, goal_config):
+
+        plan = []
+
+        # TODO (student): Implement your planner here.
+
+        plan.append(start_config)
+        plan.append(goal_config)
+
+        return numpy.array(plan)
+
+    def ShortenPath(self, path):
+
+        # TODO (student): Postprocess the planner.
+        return path
